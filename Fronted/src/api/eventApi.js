5fc06b99fb5c9ecfb718df9f54d82972ff5c697b
Gautam sagar <gateaspirant8650@gmail.com>
2025-11-02 21:18:21 +0530

    update fronted

--- Fronted/src/api/eventApi.js
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-export const getAllEvents = async () => {
-    const res = await api.get("/events");
-    return res.data;
-};
-export const filterEvents = async (params) => {
-    const res = await api.get("/events/filter", { params });
-    return res.data;
-};
